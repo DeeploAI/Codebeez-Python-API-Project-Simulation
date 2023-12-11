452a7b48b3d156e042366f96eef29af14c07d512
DeeploAI <153511608+DeeploAI@users.noreply.github.com>
2023-12-11 12:42:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,26 @@
-# app.py
-from flask import Flask, jsonify, request
+# main.py
+from fastapi import FastAPI, HTTPException
 
-app = Flask(__name__)
+app = FastAPI()
 
 # Simulated data store
 users = [{"id": 1, "name": "John Doe"}, {"id": 2, "name": "Jane Doe"}]
 
-@app.route('/users', methods=['GET'])
-def get_users():
-    return jsonify(users)
+@app.get("/users")
+def read_users():
+    return users
 
-@app.route('/user', methods=['POST'])
-def add_user():
-    # Intentional issue: Lack of input validation
-    user = request.json
-    users.append(user)
-    return jsonify(user), 201
+@app.post("/users")
+def add_user(user_id: int, name: str):
+    # Intentional issue: Lack of data validation and proper request body handling
+    new_user = {"id": user_id, "name": name}
+    users.append(new_user)
+    return new_user
 
-@app.route('/user/<int:user_id>', methods=['GET'])
-def get_user(user_id):
+@app.get("/users/{user_id}")
+def read_user(user_id: int):
     # Intentional issue: Inefficient user lookup
     for user in users:
-        if user['id'] == user_id:
-            return jsonify(user)
-    return "User not found", 404
-
-if __name__ == '__main__':
-    app.run(debug=True)
+        if user["id"] == user_id:
+            return user
+    raise HTTPException(status_code=404, detail="User not found")
